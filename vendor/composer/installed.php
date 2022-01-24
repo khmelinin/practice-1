db9ced39c00d0bdc45f28631d012037d33d713c0
AdmiN <khmelinin2001@gmail.com>
2022-01-24 20:00:27 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '443445fdeb33febdaa75741cb320fc308f5abdf2',
+        'name' => 'vendor_name/practice-1',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/practice-1' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '443445fdeb33febdaa75741cb320fc308f5abdf2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
