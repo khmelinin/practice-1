96b15fd44e21f24e0ca5c4682e45be621287c5ec
AdmiN <khmelinin2001@gmail.com>
2022-02-05 11:07:07 +0200

    added login/register pages

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/pages', [\App\Http\Controllers\PageController::class, "index"]);
 Route::get('/table', [\App\Http\Controllers\PageController::class, "table"]);
 Route::post('/pages', [\App\Http\Controllers\PageController::class, "addMessage"]);
+
+
+require __DIR__.'/auth.php';
